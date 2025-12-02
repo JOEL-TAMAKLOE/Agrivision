a88984917c2be31bfb2dc7361c134801dd6f81de
TAMAKLOE <103913308+JOEL-TAMAKLOE@users.noreply.github.com>
2025-12-01 17:39:12 -0800

    Update logo image path in show_header function

--- Apps/field_app.py
+++ Apps/field_app.py
@@ -214,7 +214,7 @@ if "dark_mode" not in st.session_state:
 # Top bar with logo + animation
 def show_header():
     app_dir = os.path.join(os.getcwd(), "images")
-    logo_path = os.path.join(app_dir, "imagehome.png")
+    logo_path = os.path.join(app_dir, "image1.png")
 
     def load_base64(path):
         with open(path, "rb") as f:
